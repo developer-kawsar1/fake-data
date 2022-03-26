f7eb54992fc67ee06cec1e219487fae8117f1df9
zislam229@gmail.com <zislam229@gmail.com>
2022-03-26 09:34:24 +0600

    first

---
+++ main.js
@@ -0,0 +1,68 @@
+let products=[
+  
+  
+  {
+    id:1,
+    name:'Steeliness Steel Watch for Mens black',
+   img:'products-images/1.jpg',
+    price:200
+    
+  },
+  {
+    id: 2,
+    name: 'Stainless Rhadu black and goldk',
+    img: 'products-images/2.jpg',
+    price: 250
+  
+  },
+  {
+    id: 3,
+    name: 'Artificial Pu Leather Watch For Men',
+    img: 'products-images/3.jpg',
+    price: 193
+  
+  },
+    {
+      id: 4,
+      name: 'Dior magnet watch for Girl 2 piece',
+      img: 'products-images/4.jpg',
+      price: 243
+  
+    },
+      {
+        id: 5,
+        name: 'useful watch',
+        img: 'products-images/5.jpg',
+        price: 123
+    
+      },
+        {
+          id: 6,
+          name: 'Artificial Pu Leather Watch For Men',
+          img: 'products-images/6.jpg',
+          price: 199
+      
+        },
+          {
+            id: 7,
+            name: 'Artificial Pu Leather Watch For Men',
+            img: 'products-images/7.jpg',
+            price: 393
+        
+          },
+            {
+              id: 8,
+              name: 'Artificial Pu Leather Watch For Men',
+              img: 'products-images/8.jpg',
+              price: 163
+          
+            },
+              {
+                id: 9,
+                name: 'Artificial Pu Leather Watch For Men',
+                img: 'products-images/9.jpg',
+                price: 243
+            
+              },
+  
+  ]

2635b1aa758591edca2b7191916f3f0e650a36d8
zislam229@gmail.com <zislam229@gmail.com>
2022-03-26 14:19:49 +0600

    updated

--- main.js
+++ main.js
@@ -1,68 +1,14 @@
-let products=[
-  
-  
-  {
-    id:1,
-    name:'Steeliness Steel Watch for Mens black',
-   img:'products-images/1.jpg',
-    price:200
+fetch('data.json')
+.then(res=>res.json())
+.then(data=>{
+  for(single of data){
+    var div=document.createElement('div');
+    div.classList.add('mydiv')
+    div.innerHTML+=`
+    <p>${single.name} </p>
+      <img src="${single.img}" alt="">
+    `
+    document.body.appendChild(div)
     
-  },
-  {
-    id: 2,
-    name: 'Stainless Rhadu black and goldk ',
-    img: 'products-images/2.jpg',
-    price: 250
-  
-  },
-  {
-    id: 3,
-    name: 'Artificial Pu Leather Watch For Men',
-    img: 'products-images/3.jpg',
-    price: 193
-  
-  },
-    {
-      id: 4,
-      name: 'Dior magnet watch for Girl 2 piece',
-      img: 'products-images/4.jpg',
-      price: 243
-  
-    },
-      {
-        id: 5,
-        name: 'useful watch',
-        img: 'products-images/5.jpg',
-        price: 123
-    
-      },
-        {
-          id: 6,
-          name: 'Artificial Pu Leather Watch For Men',
-          img: 'products-images/6.jpg',
-          price: 199
-      
-        },
-          {
-            id: 7,
-            name: 'Artificial Pu Leather Watch For Men',
-            img: 'products-images/7.jpg',
-            price: 393
-        
-          },
-            {
-              id: 8,
-              name: 'Artificial Pu Leather Watch For Men',
-              img: 'products-images/8.jpg',
-              price: 163
-          
-            },
-              {
-                id: 9,
-                name: 'Artificial Pu Leather Watch For Men',
-                img: 'products-images/9.jpg',
-                price: 243
-            
-              },
-  
-  ]
+  }
+})
